3bc0a65a97e910e8f1cab75ff91c60e16d8aa8c3
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-20 21:58:56 +0200

    Continuation Passing Style.

---
+++ ContinuationPassingStyle_UdemyAdditional_ConsoleApp/WorkflowResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContinuationPassingStyle_UdemyAdditional_ConsoleApp
+{
+    public enum WorkflowResult
+    {
+        Success,
+        Failure
+    }
+}
