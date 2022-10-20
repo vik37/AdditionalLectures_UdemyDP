43efdf92edf065b99aca16e0bf55e687a6b71222
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-20 20:49:59 +0200

    ASCII string.

---
+++ Testing/Ascii/Ascii_Test.cs
@@ -0,0 +1,47 @@
+﻿using AsciiString_UdemyDPAdditional_ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testing.Ascii
+{
+    public class Ascii_Test
+    {
+        private string text = "testing!";
+        [Fact]
+        public void Test_ASCII()
+        {
+            var constructed = new str(text);
+            Assert.Equal(text,constructed.ToString());
+
+            str assigned = text;
+            Assert.Equal(assigned.ToString(), text);
+        }
+        [Fact]
+        public void ComparisonTest()
+        {
+            str first = text;
+            str second = text;
+
+            // Equals
+            Assert.Equal(second, first);
+            // operation ==
+            Assert.True(first == second);
+            // operation == with string
+            Assert.Equal(text, second);
+        }
+        [Fact]
+        public void ConcatenationTest()
+        {
+            str foo = "foo";
+            str bar = "bar";
+            Assert.Equal("foobar", foo + bar);
+            Assert.Equal("foobar", "foo" + bar);
+            Assert.Equal("foobar", foo + "bar");
+
+            foo += bar;
+            Assert.Equal("foobar", foo);
+        }
+    }
+}
